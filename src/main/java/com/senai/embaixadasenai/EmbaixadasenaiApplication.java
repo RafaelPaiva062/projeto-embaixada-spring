223c3d69d80743cf1682afa211208b1d74b59ed9
CarlosCafeJs <carloscafe.js@gmail.com>
2024-11-25 10:00:26 -0300

    Adicinando fase incial do projeto

---
+++ src/main/java/com/senai/embaixadasenai/EmbaixadasenaiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.embaixadasenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbaixadasenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbaixadasenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/embaixadasenai/EmbaixadasenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.embaixadasenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbaixadasenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
